c1a617edde82d57e860762a5b49920b42695787b
RostikMoroziuk <rmorozyukua@gmail.com>
2017-04-18 00:17:38 +0300

    Fix bug with transition links

--- js/links.js
+++ js/links.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $("#menu").on("click", "a", function(event) {
+    $("#nav-menu").on("click", "a", function(event) {
         event.preventDefault();
         var id = $(this).attr('href'),
-            top = $(id).offset().top;
+            top = $(id).offset().top-180;
         $('body,html').animate({ scrollTop: top }, 1500);
     });
 });
