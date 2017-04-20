9c84cd91fb758acb381dd5c0dad4e0fc06bab33e
RostikMoroziuk <rmorozyukua@gmail.com>
2017-04-20 10:23:16 +0300

    Add footer

--- js/links.js
+++ js/links.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#nav-menu").on("click", "a", function(event) {
         event.preventDefault();
         var id = $(this).attr('href'),
-            top = $(id).offset().top-180;
+            top = $(id).offset().top-$( window ).height()*0.15;
         $('body,html').animate({ scrollTop: top }, 1500);
     });
 });
